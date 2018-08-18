5c23af52450228c790f326b26fdaafe05581eb06
Josh Mobley <josh@joshmobley.net>
2018-08-18 15:29:42 -0400

    adding copy-to-clipboard for simple clipboard functionality

--- src/components/TableShelf.js
+++ src/components/TableShelf.js
@@ -1,24 +1,11 @@
 import React from 'react';
+import copy from 'copy-to-clipboard';
 
 class TableShelf extends React.Component {
 
-  copyValue(event, index) {
+  copyValue(event, value) {
     event.preventDefault();
-
-    var emailLink = document.querySelector('#linkable-0');
-    var range     = document.createRange();
-    range.selectNode(emailLink);
-    window.getSelection().addRange(range);
-    var successful = document.execCommand('copy');
-
-    /*
-    var node = document.querySelector('#linkable-' + index);
-    var range = document.createRange();
-    range.selectNode(node);
-    window.getSelection().addRange(range);
-    console.log(window.getSelection());
-    document.execCommand('copy');
-    */
+    copy(value);
   }
 
   render() {
@@ -29,22 +16,22 @@ class TableShelf extends React.Component {
 
     return (
       <div className="table__shelf">
-      { sections.map( section =>
-          <div key={section.name}>
-            <h4 className="table__sub-header">{section.name}</h4>
-            <div className="table__section">
-              { section.data.map( (data, index) =>
-                <dl className="flex mb-4" key={ data.label }>
-                  <dt className="bold">{ data.label }</dt>
-                  <dd className="table__linkable" id={"linkable-" + index}>
-                    { data.value }
-                    <a className="ml-2 text-small" href="#copy" onClick={(event) => this.copyValue(event, index)}>Copy</a>
-                  </dd>
-                </dl>
-              )}
+        { sections.map( section =>
+            <div key={section.name}>
+              <h4 className="table__sub-header">{section.name}</h4>
+              <div className="table__section">
+                { section.data.map( (data, index) =>
+                  <dl className="flex mb-4" key={ data.label }>
+                    <dt className="bold">{ data.label }</dt>
+                    <dd className="table__linkable">
+                      { data.value }
+                      <a className="ml-2 text-small" href="#copy" onClick={(event) => this.copyValue(event, data.value)}>Copy</a>
+                    </dd>
+                  </dl>
+                )}
+              </div>
             </div>
-          </div>
-      )}
+        )}
       </div>
     )
   }
