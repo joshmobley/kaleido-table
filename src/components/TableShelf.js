442fd3734ffb8bc229a78985b2ef4dea80949b44
Josh Mobley <josh@joshmobley.net>
2018-08-18 11:13:34 -0400

    WIP: beginning to add copy to clipboard functionality for values in table

--- src/components/TableShelf.js
+++ src/components/TableShelf.js
@@ -2,6 +2,25 @@ import React from 'react';
 
 class TableShelf extends React.Component {
 
+  copyValue(event, index) {
+    event.preventDefault();
+
+    var emailLink = document.querySelector('#linkable-0');
+    var range     = document.createRange();
+    range.selectNode(emailLink);
+    window.getSelection().addRange(range);
+    var successful = document.execCommand('copy');
+
+    /*
+    var node = document.querySelector('#linkable-' + index);
+    var range = document.createRange();
+    range.selectNode(node);
+    window.getSelection().addRange(range);
+    console.log(window.getSelection());
+    document.execCommand('copy');
+    */
+  }
+
   render() {
 
     const {
@@ -14,10 +33,13 @@ class TableShelf extends React.Component {
           <div key={section.name}>
             <h4 className="table__sub-header">{section.name}</h4>
             <div className="table__section">
-              { section.data.map( data =>
+              { section.data.map( (data, index) =>
                 <dl className="flex mb-4" key={ data.label }>
                   <dt className="bold">{ data.label }</dt>
-                  <dd>{ data.value }</dd>
+                  <dd className="table__linkable" id={"linkable-" + index}>
+                    { data.value }
+                    <a className="ml-2 text-small" href="#copy" onClick={(event) => this.copyValue(event, index)}>Copy</a>
+                  </dd>
                 </dl>
               )}
             </div>
