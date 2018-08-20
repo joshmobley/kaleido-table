b7b0160027c5ab9fa2e6febe46c2e82383eb1e50
Josh Mobley <josh@joshmobley.net>
2018-08-19 22:52:27 -0400

    WIP: progress with dockerized replica set, adding mongoose, and using demo change stream

--- server/server.js
+++ server/server.js
@@ -1,30 +1,71 @@
 const express = require('express');
-const http = require('http');
 const mongo   = require('mongodb').MongoClient;
-const socketIO = require('socket.io');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-/*const dbUrl = 'mongodb://kaleido-table-db:27017/kaleido';
 
-mongo.connect(dbUrl, (err, client) => {
-  if( err ) throw err;
-  var db = client.db('kaleido');
-  const nodes = db.collection('nodes');
-  const changeStream = nodes.watch();
-  const next = await changeStream.next();
-});*/
+const dbUrl = 'mongodb://mongodb-primary:27017';
 
-const app   = express();
-const port  = process.env.PORT || 5000;
+const pipeline = [
+  {
+    $project: { documentKey: false }
+  }
+];
 
-const server = http.createServer(app);
-const io = socketIO(server);
+mongo.connect(dbUrl).then(client => {
+  console.log('connected to db server');
+  const db = client.db('kaleido');
+  db.createCollection("nodes");
+  const collection = db.collection('nodes');
+  const changeStream = collection.watch(pipeline);
+  // start listen to changes
+      changeStream.on("change", function(change) {
+        console.log(change);
+      });
+});
 
-io.on('connection', socket => {
-  console.log('user connected');
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+
+const app = express();
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+})
+
+mongoose.connect(dbUrl + '/kaleido');
+
+var nodeSchema = new mongoose.Schema({
+  id: String,
+  header: String,
+  netStatus: Boolean,
+  logStatus: Boolean,
+  vpcPublic: Boolean,
+  vpcPrivate: Boolean,
+  sections: Array
+});
+
+var Node = mongoose.model("Node", nodeSchema);
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  Node.find((err, nodes) => {
+
+    console.log(nodes);
+    res.send(nodes);
   });
 });
 
-server.listen(port, () => console.log(`Listening on port ${port}`))
+app.post("/node", (req, res) => {
+  console.log(req.body);
+  var data = new Node(req.body);
+  data.save().then( item => {
+    res.send("node saved");
+  })
+  .catch( err => {
+    res.status(400).send('unable to save to db');
+  });
+});
